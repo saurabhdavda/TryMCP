3f1444a6f82b2375a5a2057842f84b06f184ed39
Saurabh Davda <saurabh.davda@simformsolutions.com>
2026-03-03 14:38:51 +0530

    MCP Server working with using it in an Agent

---
+++ TryMCP/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Logging.AddConsole(consoleLogOptions =>
+{
+    consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+});
+
+builder.Services
+        .AddMcpServer()
+        .WithStdioServerTransport()
+        .WithToolsFromAssembly();
+await builder.Build().RunAsync();
+
+[McpServerToolType]
+public static class EchoTool
+{
+    [McpServerTool, Description("Echoes the message back to the client.")]
+    public static string Echo(string message) => $"hello {message}";
+    
+    [McpServerTool, Description("Returns the length of the message.")]
+    public static int Length(string message) => message.Length;
+}
